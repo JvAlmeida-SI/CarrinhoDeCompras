436cd6e7a4c985292a22051bca0f2c03accbba80
JvAlmeida <almeida.victor@ufvjm.edu,br>
2023-09-27 11:59:35 -0300

    List learning commit in Java

--- src/main/Item.java
+++ src/main/Item.java
@@ -15,23 +15,11 @@ public class Item {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public double getPreco() {
         return preco;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
     public int getQuantidade() {
         return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
     }
 }
